59cba3e1e9977b1d62607d445adfe8ddd9d39536
mcal25 <225284010+mcal25@users.noreply.github.com>
2025-09-17 18:04:24 -0400

    Update calculator visual appearance

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log('bee happy 🐝');
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let num1 = 2;
+
+let num2 = 3;
+
+// let operator = '+';
+
+function operate(num1, operator, num2) {
+    if (operator == 'add') {
+        return add(num1, num2);
+    }
+}
+
+console.log(operate(num1, 'add', num2));
