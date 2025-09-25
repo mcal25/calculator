44a552dad7b744e4413906ae760a753d4cd64805
mcal25 <225284010+mcal25@users.noreply.github.com>
2025-09-25 19:14:45 -0400

    Improve event listeners

--- script.js
+++ script.js
@@ -20,12 +20,53 @@ let num1 = 2;
 
 let num2 = 3;
 
-// let operator = '+';
+let currentTotal = 0;
+
+let operator = '+';
 
 function operate(num1, operator, num2) {
-    if (operator == 'add') {
+    if (operator == '+') {
         return add(num1, num2);
+    } else if (operator == '-') {
+        return subtract(num1, num2);
+    } else if (operator == '*') {
+        return multiply(num1, num2);
+    } else if (operator == '/') {
+        return divide(num1, num2);
     }
 }
 
 console.log(operate(num1, 'add', num2));
+
+
+function applyEventListeners() {
+    let numberButtons = document.querySelectorAll('.number-button');
+    let operatorButtons = document.querySelectorAll('.operator-button');
+    // let equalsButton = document.querySelectorAll('#=');
+    // let clrButton = document.querySelectorAll('#clr');
+
+    numberButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            console.log(button.innerHTML)
+            document.querySelector('#display-number').innerHTML += button.innerHTML;
+        })
+    });
+
+    operatorButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            console.log(button.innerHTML)
+        })
+    });
+}
+
+applyEventListeners();
+
+// numButtonFunction(button, eventlistener) {
+//     // adds event listener that will populate displayNumber div with the current number
+// }
+
+
+// document.getElementById('display-number').textContent = '235235';
+
+
+// console.log(document.querySelectorAll('.number-button').forEach(div => console.log(div.innerHTML)));
